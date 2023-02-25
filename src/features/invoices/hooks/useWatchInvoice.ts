4d55a1997acc6d52de0d282e51914d361457a859
chazzlee <l.chazz00@gmail.com>
2023-02-25 13:17:09 -0500

    cleanup

--- src/features/invoices/hooks/useWatchInvoice.ts
+++
@@ -1,6 +0,0 @@
-import { useWatch, type FieldPath } from "react-hook-form";
-import type { InvoiceFormData } from "../types";
-
-export function useWatchInvoice<K extends FieldPath<InvoiceFormData>>(name: K) {
-  return useWatch<InvoiceFormData, K>({ name });
-}
